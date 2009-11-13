7720623066276a99e9f539747c2e3f064c00c63a
damianferrai <damianferrai@c6cceab8-d018-11de-bb34-65c810889981>
2009-11-13 05:56:44 +0000

    Primer esbozo de interfaz, en silverlight

---
+++ code/GHDoctor/GHDoctor/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace GHDoctor
+{
+	public partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+            
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+            Grid mainView = (Grid)App.Current.RootVisual;
+            mainView.Children.Clear();
+            mainView.Children.Add(new AboutPage());
+		}
+	}
+}
