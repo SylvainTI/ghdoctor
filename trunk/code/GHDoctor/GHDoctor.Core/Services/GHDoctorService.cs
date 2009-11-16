baa517f74e3341fb96fba8a9e7436bdcb1ea31c1
nacho.varela <nacho.varela@c6cceab8-d018-11de-bb34-65c810889981>
2009-11-16 04:13:55 +0000

    - Agregué proyecto Core que incluye modelo LINQ to SQL de la base. - Script de creación de la base de datos. - Scripts que mandó Lio. - Clase de servicios que recibe una lista de commonquery y ejecuta la consulta para un sitio determinado.

---
+++ trunk/code/GHDoctor/GHDoctor.Core/Services/GHDoctorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace GHDoctor.Core.Services
+{
+    public class GHDoctorService
+    {
+        // Pensar bien como tiene que ser este servicio, va a ser consumido desde un web service 
+        // que luego la aplicación Silverlight va a usar.
+
+        public void GetResults(IList<CommonQuery> queries, string siteName)
+        {
+            if (String.IsNullOrEmpty(siteName))
+                throw new Exception("Se debe especificar nombre del sitio.");
+
+            foreach (var query in queries)
+            {
+                Search(query.SearchString, siteName);
+
+            }        
+        }
+
+        private void Search(string query, string siteName)
+        {
+            WebClient webClient = new WebClient();
+            string address = @"http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" +
+                query + " site:" + siteName;
+            string result = webClient.DownloadString(address);
+        }
+    }
+}
