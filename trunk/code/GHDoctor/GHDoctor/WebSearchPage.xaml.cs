4db0f7cde4c268e563081d97226715d05b7b0aed
damianferrai <damianferrai@c6cceab8-d018-11de-bb34-65c810889981>
2009-11-19 13:57:40 +0000

    Listo el esqueleto de la UI

---
+++ trunk/code/GHDoctor/GHDoctor/WebSearchPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace GHDoctor
+{
+    public partial class WebSearchPage : Page
+    {
+        public WebSearchPage()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+		
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Grid mainView = (Grid)App.Current.RootVisual;
+            mainView.Children.Clear();
+            mainView.Children.Add(new MainPage());
+        }
+
+		private void Buscar_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+            Grid mainView = (Grid)App.Current.RootVisual;
+            mainView.Children.Clear();
+            mainView.Children.Add(new WebSearchPageResults());
+		}
+    }
+}
