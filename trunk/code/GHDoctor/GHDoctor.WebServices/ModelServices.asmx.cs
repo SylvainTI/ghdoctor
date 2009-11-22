11203ae7d0b09a4cd85dee9daa16f2dad54df2e5
damianferrai <damianferrai@c6cceab8-d018-11de-bb34-65c810889981>
2009-11-22 23:59:19 +0000

    Conexion entre sl y back. Se llenan dos combobox

---
+++ trunk/code/GHDoctor/GHDoctor.WebServices/ModelServices.asmx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using GHDoctor.Core.Services;
+using GHDoctor.Core;
+using System.Text;
+using System.Xml;
+
+namespace GHDoctor.WebServices
+{
+    /// <summary>
+    /// Summary description for ModelServices
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Aca van los web services que devuelven cosas del modelo
+    public class ModelServices : System.Web.Services.WebService 
+    {
+        [WebMethod]
+        public List<Category> GetAllCategories()
+        {
+            GHDoctorService ghSvc = new GHDoctorService();
+            //IList<Category> ret = ghSvc.GetAllCategories().ToList<Category>();
+
+
+/*
+            DataContractSerializer dcs = new DataContractSerializer(typeof(Category));
+            StringBuilder sb = new StringBuilder();
+            XmlWriter writer = XmlWriter.Create(sb);
+            dcs.WriteObject(writer, cust);
+            writer.Close();
+            string xml = sb.ToString();
+            */
+
+            return ghSvc.GetAllCategories().ToList<Category>();
+        }
+    }
+}
---
+++ trunk/code/GHDoctor/GHDoctor.Website/WebServices/ModelServices.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using GHDoctor.Core.Services;
+using GHDoctor.Core;
+using System.Text;
+using System.Xml;
+
+namespace GHDoctor.WebServices
+{
+    /// <summary>
+    /// Summary description for ModelServices
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Aca van los web services que devuelven cosas del modelo
+    public class ModelServices : System.Web.Services.WebService 
+    {
+        [WebMethod]
+        public List<Category> GetAllCategories()
+        {
+            GHDoctorService ghSvc = new GHDoctorService();
+            return ghSvc.GetAllCategories().ToList<Category>();
+        }
+
+        [WebMethod]
+        public List<CommonQuery> GetCommonQueries(int categoryCode)
+        {
+            GHDoctorService ghSvc = new GHDoctorService();
+            return ghSvc.GetCommonQueries(categoryCode).ToList<CommonQuery>();
+        }
+    }
+}
