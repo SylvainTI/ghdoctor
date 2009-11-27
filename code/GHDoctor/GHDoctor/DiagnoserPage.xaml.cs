c6fbc99abdeb573c14b6754992d251e00104f83e
damianferrai <damianferrai@c6cceab8-d018-11de-bb34-65c810889981>
2009-11-27 05:50:47 +0000

    Hice algo del Diagnosticar. Ojo nacho cuidado si vas a hacer update, porque modifique los localhost por dalef.dyndns.org. Si agregas al archivo "c:\windows\system32\drivers\hosts" la linea "127.0.0.1 dalef.dyndns.org", te deberia andar

--- code/GHDoctor/GHDoctor/DiagnoserPage.xaml.cs
+++ code/GHDoctor/GHDoctor/DiagnoserPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using GHDoctor.ModelServicesReference;
 
 namespace GHDoctor
 {
@@ -18,6 +19,19 @@ namespace GHDoctor
         public DiagnoserPage()
         {
             InitializeComponent();
+
+            ModelServicesSoapClient svc = new ModelServicesSoapClient();
+            svc.GetAllCategoriesCompleted += new EventHandler<GetAllCategoriesCompletedEventArgs>(svc_GetAllCategoriesCompleted);
+            svc.GetAllCategoriesAsync();
+
+            ErrorMsgTxt.Visibility = Visibility.Collapsed;
+            
+        }
+
+        void svc_GetAllCategoriesCompleted(object sender, GetAllCategoriesCompletedEventArgs e)
+        {
+            QueryTypes.ItemsSource = e.Result;
+            QueryTypes.DisplayMemberPath = "ShortDescription";
         }
 
         // Executes when the user navigates to this page.
@@ -39,10 +53,36 @@ namespace GHDoctor
         {
             if (this._contentLoaded)
             {
-                Grid mainView = (Grid)App.Current.RootVisual;
-                mainView.Children.Clear();
-                mainView.Children.Add(new DiagnoserResultsPage());
+                if (QueryTypes.SelectedItems.Count == 0)
+                {
+                    ErrorMsgTxt.Text = "Seleccione al menos una categoria para la búsqueda";
+                    ErrorMsgTxt.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    ErrorMsgTxt.Visibility = Visibility.Collapsed;
+
+                    Grid mainView = (Grid)App.Current.RootVisual;
+                    mainView.Children.Clear();
+                    mainView.Children.Add(new DiagnoserResultsPage());
+                }
             }
+        }
+
+        private void SelectAllQueries_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            foreach (Category category in QueryTypes.Items)
+            {
+                QueryTypes.SelectedItems.Add(category);
+            }
+        }
+
+        private void QueryTypes_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+			    if (QueryTypes.SelectedItems.Count > 0)
+                {
+		        	ErrorMsgTxt.Visibility = Visibility.Collapsed;
+				}
         }
     }
 }
